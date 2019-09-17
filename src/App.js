b7d99d770202b2ec2838fb53e9bfa0d026542e14
Renato Perez <renatoperezc@gmail.com>
2019-09-17 16:55:12 -0600

    Transform Class App to function App with Hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./componentes/Home/Home";
 import MainDash from "./componentes/Main_Dash/MainDash";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      setUser: this.setCurrentUser,
-      isAValidPin: this.isAValidPin
-    };
-  }
+function App() {
 
-  setCurrentUser = user => this.setState({ user });
-  isAValidPin = pin => /\d{4}/.test(pin);
+  const [user, setUser] = useState(null);
+  const isAValidPin = pin => /\d{4}/.test(pin);
 
-  render() {
-    return (
-      <Switch>
-        <Route
-          path="/main"
-          render={rp => <MainDash {...rp} {...this.state} />}
-        />
-        <Route exact path="/" render={rp => <Home {...this.state} {...rp} />} />
-      </Switch>
-    );
-  }
+  return (
+    <Switch>
+      <Route
+        path="/main"
+        render={routerProps => (
+          <MainDash {...routerProps} {...{ user, setUser, isAValidPin }} />
+        )}
+      />
+      <Route
+        exact
+        path="/"
+        render={routerProps => (
+          <Home {...routerProps} {...{ user, setUser, isAValidPin }} />
+        )}
+      />
+    </Switch>
+  );
 }
 
 export default App;
