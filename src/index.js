a1027c69312fc56d416d5629d4a9417a723d4b79
Renato Perez <renatoperezc@gmail.com>
2019-09-20 09:03:37 -0600

    Move Router from index to App

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import "./styles/tailwind.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
