e85193b5ae86ef0103a8e150054fc96e31222c5f
Renato Perez <renatoperezc@gmail.com>
2019-09-20 07:39:10 -0600

    Move Router from index to App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "./componentes/Home/Home";
 import MainDash from "./componentes/Main_Dash/MainDash";
 
 function App() {
-
   const [user, setUser] = useState(null);
   const isAValidPin = pin => /\d{4}/.test(pin);
 
   return (
-    <Switch>
-      <Route
-        path="/main"
-        render={routerProps => (
-          <MainDash {...routerProps} {...{ user, setUser, isAValidPin }} />
-        )}
-      />
-      <Route
-        exact
-        path="/"
-        render={routerProps => (
-          <Home {...routerProps} {...{ user, setUser, isAValidPin }} />
-        )}
-      />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route
+          path="/main"
+          render={routerProps => (
+            <MainDash {...routerProps} {...{ user, setUser, isAValidPin }} />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={routerProps => (
+            <Home {...routerProps} {...{ user, setUser, isAValidPin }} />
+          )}
+        />
+      </Switch>
+    </Router>
   );
 }
 
